256a211c7615411f5b4283c5a7ead32244e60432
ananin <sergioblancoxdd@gmail.com>
2020-10-10 21:42:22 +0200

    Revert "One more"
    This reverts commit 9a2072d7e310cb3d166db9c7ee5cfa5ddb2d5264.

--- febrer.php
+++ febrer.php
@@ -1,4 +1,3 @@
 echo'per febrer, carnestoltes el primer';
 echo'pel febrer flor a lametller';
 echo'mes tard o mes matiner carnaval dins el febrer';
-echo'Has de llaurar al febrer el que al marc vulgues sembrar';
