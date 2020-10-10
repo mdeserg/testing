9552f9011fe08e8723a5aa77c6b882d5b32ff529
ananin <sergioblancoxdd@gmail.com>
2020-10-10 22:27:59 +0200

    Nous canvis per nova etiqueta

--- estudiants_form.php
+++ estudiants_form.php
@@ -1,1 +1,2 @@
 Alta estudiantes
+dades estudiants
--- febrer.php
+++ febrer.php
@@ -1,3 +1,4 @@
 echo'per febrer, carnestoltes el primer';
 echo'pel febrer flor a lametller';
 echo'mes tard o mes matiner carnaval dins el febrer';
+espai gatronòmic
