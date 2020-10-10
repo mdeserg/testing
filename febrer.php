59b3a8ecb5dd5f4a35b421dded8d914dd795dc1f
ananin <sergioblancoxdd@gmail.com>
2020-10-10 21:41:15 +0200

    One more

--- febrer.php
+++ febrer.php
@@ -1,3 +1,4 @@
 echo'per febrer, carnestoltes el primer';
 echo'pel febrer flor a lametller';
 echo'mes tard o mes matiner carnaval dins el febrer';
+echo'Has de llaurar al febrer el que al marc vulgues sembrar';
